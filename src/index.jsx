61d785a44c652363dec91807327e7b92c44dc212
Xiaoguo Liu <liu_xiao_guo@yahoo.com>
2021-02-04 18:07:01 +0800

    Removed APM related code

--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app";
 
-import { init as initApm } from '@elastic/apm-rum'
-var apm = initApm({
-
-  // Set required service name (allowed characters: a-z, A-Z, 0-9, -, _, and space)
-  serviceName: 'weather',
-
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://localhost:8200',
-
-  // Set service version (required for source map feature)
-  serviceVersion: '1'
-})
-
 ReactDOM.render(
     <React.StrictMode>
         <App />
